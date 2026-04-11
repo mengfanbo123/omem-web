699e814d4de1e2c0e6351a797465d8ce747b228f
mengfanbo123 <mengfanbo123@users.noreply.github.com>
2026-04-11 18:03:50 +0800

    fix: add minimal src files for dev server

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>omem-web</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
