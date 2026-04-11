85a47015a14f1b5af24dbc247b4fe6f11400b557
mengfanbo123 <mengfanbo123@users.noreply.github.com>
2026-04-11 18:44:13 +0800

    feat: add utility functions and update entry files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app">
-    <h1>omem-web</h1>
-  </div>
+  <router-view />
 </template>
 
 <script setup lang="ts">
+// App root component
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
