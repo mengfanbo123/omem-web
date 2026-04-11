2369090dd95075e18bb0b081260c2c634de35fa1
mengfanbo123 <mengfanbo123@users.noreply.github.com>
2026-04-11 19:46:14 +0800

    fix: 注册 Ant Design Vue 组件库

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +13,6 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
